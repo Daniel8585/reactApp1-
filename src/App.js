30d4f4148c91c9f296e918f9413d5922dd10d3f7
unknown <daniel.dallakyan@gmail.com>
2020-06-19 00:10:27 +0400

    sxalmunqi patcharov chem are commit-ner@

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+    
+  }
+  plus = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
 
+  minus = () => {
+    this.setState({
+      count: this.state.count - 1,
+    })
+  }
+  render() {
+    return (
+      
+        
+        
+  <div className="App">
+  <header className="App-header">
+    <div className='add'>
+        <img src='https://image.flaticon.com/icons/png/512/16/16909.png' width='100px' height='100px'onClick={this.plus}/>
+            <div className='count'> {this.state.count} </div>
+            
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Feather-core-minus-circle.svg/1024px-Feather-core-minus-circle.svg.png' width='100px' height='100px'onClick={this.minus}/>
+        </div> 
+        <h1>React App 1</h1>
+   </header>
+ </div>
+      
+    )
+  }
+}
 export default App;
+
+
+
+
+
